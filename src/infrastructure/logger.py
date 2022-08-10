8ad2e53ad97ecd2ac786af862d24e00e9cdf7e9b
Marius Vorster <mariusv@inoxico.com>
2022-08-10 20:59:31 +0200

    Logger refactor - Make rolling type configurable

--- src/infrastructure/logger.py
+++ src/infrastructure/logger.py
@@ -9,46 +9,55 @@ from logging.handlers import TimedRotatingFileHandler
 from logging import config
 from src.common.locations import Locations
 from src.common.singleton_decorator import Singleton
+from enum import Enum
+
+class RollingFileMethod(Enum):
+    ByMaxSize = 1
+    Daily = 2
 
 class LogManager(metaclass=Singleton):
         _logger = None
+        _name = 'app'
+        _rolling_file_method = RollingFileMethod.ByMaxSize
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", '%m-%d-%Y %H:%M:%S')
         
         @property
         def logger(self) -> logging.Logger:
                 return self._logger
 
-        def __init__(self) -> None:
-                print('Initializing logging')
-
-                # Main logger
-                self._logger = logging.getLogger()
-                self._logger.setLevel(logging.INFO)
-
-                formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", '%m-%d-%Y %H:%M:%S')
-
-                # ============ Daily Rolling File Handler ============        
-                log_file_daily_path = f'{Locations.root_path()}/logs/daily_app.log'
-                file_handler_daily = TimedRotatingFileHandler(
+        def register_rolling_file_handler_daily(self, file_name):
+                log_file_daily_path = f'{Locations.root_path()}/logs/{file_name}.log'
+                handler = TimedRotatingFileHandler(
                         filename=log_file_daily_path,
                         when="D",  # D: daily | H: hourly | M: minutes
                         interval=1, # Every X days/hours/minutes
                         backupCount=20) # For X number of files
-                file_handler_daily.setFormatter(formatter)
-                file_handler_daily.setLevel(logging.INFO)
-                file_handler_daily.suffix = "%Y-%m-%d_%H_%M"
+                handler.setFormatter(self.formatter)
+                handler.setLevel(logging.INFO)
+                handler.suffix = "%Y-%m-%d_%H_%M"
                 # file_handler_daily.extMatch = re.compile(r"^\d{8}$")
 
-                self._logger.addHandler(file_handler_daily)                
+                return handler
 
-                # ============ Rolling by Size File Handler ============
-                log_file_size_path = f'{Locations.root_path()}/logs/size_app.log'
-                file_handler_size = handlers.RotatingFileHandler(
+        def register_rolling_file_handler_by_size(self, file_name):
+                log_file_size_path = f'{Locations.root_path()}/logs/{file_name}.log'
+                handler = handlers.RotatingFileHandler(
                         log_file_size_path, 
                         maxBytes=10000000, # Maximum file size
                         backupCount=20) # For X number of files
-                file_handler_size.setFormatter(formatter)
-                file_handler_size.setLevel(logging.INFO)
+                handler.setFormatter(self.formatter)
+                handler.setLevel(logging.INFO)
 
-                self._logger.addHandler(file_handler_size)
+                return handler
 
-                print('Logging initiated')
+
+        def __init__(self) -> None:
+
+                self._logger = logging.getLogger()
+                self._logger.setLevel(logging.INFO) ## Only log entries from this level or higher
+
+                if (self._rolling_file_method == RollingFileMethod.ByMaxSize):
+                        self._logger.addHandler(self.register_rolling_file_handler_by_size(self._name))
+
+                if (self._rolling_file_method == RollingFileMethod.Daily):
+                        self._logger.addHandler(self.register_rolling_file_handler_daily(self._name))
